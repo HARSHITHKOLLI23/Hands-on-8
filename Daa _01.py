e95963b711ad36ad38fe9cbf90ff33c4fd773046
HARSHITHKOLLI23 <157554815+HARSHITHKOLLI23@users.noreply.github.com>
2024-03-19 21:36:48 -0500

    Update Daa _01.py

--- Daa _01.py
+++ Daa _01.py
@@ -1,32 +1,32 @@
 
 import random
-def partition(array, lower, higher):
+def partition(a, l, h):
     pivot = array[higher]
-    i = lower - 1
-    for j in range(lower, higher):
+    i = l - 1
+    for j in range(l, h):
         if array[j] <= pivot:
             i += 1
             array[i], array[j] = array[j], array[i]
-    array[i + 1], array[higher] = array[higher], array[i + 1]
+    array[i + 1], array[h] = array[h], array[i + 1]
     return i + 1
 
-def quickselect(array, lower, higher, i):
-    if lower == higher:
-        return array[lower]
-    pivot_index = partition(array, lower, higher)
-    k = pivot_index - lower + 1
+def quickselect(a, l, h, i):
+    if l == h:
+        return array[l]
+    pivot_index = partition(a, l, h)
+    k = pivot_index - l + 1
     if i == k:
         return array[pivot_index]
     elif i < k:
-        return quickselect(array, lower, pivot_index - 1, i)
+        return quickselect(a, l, pivot_index - 1, i)
     else:
-        return quickselect(array, pivot_index + 1, higher, i - k)
+        return quickselect(a, pivot_index + 1, h, i - k)
 
-def ith_order_statistic(array, i):
-    if i < 1 or i > len(array):
+def ith_order_statistic(a, i):
+    if i < 1 or i > len(a):
         return None
-    return quickselect(array, 0, len(array) - 1, i)
+    return quickselect(a, 0, len(a) - 1, i)
 
-array = [4,8,12,18,22,29,31,35,23,54,68]
+a = [4,8,12,18,22,29,31,35,23,54,68]
 i = 7
-print(f"The {i}th order statistic of {array} is: {ith_order_statistic(array, i)}")
+print(f"The {i}th order statistic of {a} is: {ith_order_statistic(a, i)}")
